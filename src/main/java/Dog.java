a1dea6437e98401fe3090b62a98ed05dc42c406e
23kmccarrick <23kmccarrick@athenian.org>
2020-10-30 23:18:24 +0000

    dog methods

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,58 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
+    private String name;
+    private int age;
 
     // Constructors
-    
+    public Dog(){
+        this.name = "mayrock";
+        this.breed = "German Shepard";
+        this.age = 8;
+    }
+    public Dog(String name, int age, String breed){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+
+    }
 
     // makeNoise() method
-
+    public void makeNoise(){
+        System.out.println("beep beep");
+    }
 
     // toString method
-
+    public String toString(){
+        return name + " " + age + " " + breed;
+    }
 
     // Getter
-
+    public String getName(){
+        return this.name;
+    }
+    public String getBreed(){
+        return this.breed;
+    }
+    public int getAge(){
+        return this.age;
+    }
 
     // Setter
+    public void setName(String name) {
+        if (name.trim().length() != 0) {
+            this.name = name;
+        }
+    }
+    public void setAge(int age) {
+        // Don't allow negative ages
+        if (age >= 0) {
+            this.age = age;
+        }
+    }
+    public void setBreed(String breed){
+        if(breed.trim().length() != 0){
+            this.breed = breed;
+        }
+    }
 }
